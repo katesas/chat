dab04a88fadf94c0d16ac226d3487b41442a6f77
katesas <katesasidorova@gmail.com>
2023-10-25 01:07:12 +0300

    переделка

---
+++ index.js
@@ -0,0 +1,154 @@
+import express from 'express'
+import { Server } from "socket.io"
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PORT = process.env.PORT || 3500
+const ADMIN = "Admin"
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
+
+// state 
+const UsersState = {
+    users: [],
+    setUsers: function (newUsersArray) {
+        this.users = newUsersArray
+    }
+}
+
+const io = new Server(expressServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
+})
+
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`)
+
+    //  Приветствие пользователя при присоединении
+    socket.emit('message', buildMsg(ADMIN, "Добро пожаловать в Чат!"))
+
+    socket.on('enterRoom', ({ name, room }) => {
+
+        // покидание чата 
+        const prevRoom = getUser(socket.id)?.room
+
+        if (prevRoom) {
+            socket.leave(prevRoom)
+            io.to(prevRoom).emit('message', buildMsg(ADMIN, `пользователь ${name}, покинул чат`))
+        }
+
+        const user = activateUser(socket.id, name, room)
+
+        // Невозможно обновить список пользователей предыдущей комнаты до тех пор, пока не будет обновлено состояние активации пользователя. 
+        if (prevRoom) {
+            io.to(prevRoom).emit('userList', {
+                users: getUsersInRoom(prevRoom)
+            })
+        }
+
+        // присоединение 
+        socket.join(user.room)
+
+        // сообщение для пользователя в момент подключения
+        socket.emit('message', buildMsg(ADMIN, `Вы присоединились к чату - ${user.room}`))
+
+        // оповещеине остальных пользователей, что просоединился новый user
+        socket.broadcast.to(user.room).emit('message', buildMsg(ADMIN, `Пользователь ${user.name}, присоединился к чату`))
+
+        // обновление списка пользователей в чате
+        io.to(user.room).emit('userList', {
+            users: getUsersInRoom(user.room)
+        })
+
+        // Update rooms list for everyone 
+        io.emit('roomList', {
+            rooms: getAllActiveRooms()
+        })
+    })
+
+    // Оповещаем всех, когда пользователь покинул чат 
+    socket.on('disconnect', () => {
+        const user = getUser(socket.id)
+        userLeavesApp(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', buildMsg(ADMIN, `Пользователь ${user.name}, покинул чат`))
+
+            io.to(user.room).emit('userList', {
+                users: getUsersInRoom(user.room)
+            })
+
+            io.emit('roomList', {
+                rooms: getAllActiveRooms()
+            })
+        }
+
+        console.log(`User ${socket.id} disconnected`)
+    })
+
+    // прослушивание сообщений
+    socket.on('message', ({ name, text }) => {
+        const room = getUser(socket.id)?.room
+        if (room) {
+            io.to(room).emit('message', buildMsg(name, text))
+        }
+    })
+
+    // Прослушка активности 
+    socket.on('activity', (name) => {
+        const room = getUser(socket.id)?.room
+        if (room) {
+            socket.broadcast.to(room).emit('activity', name)
+        }
+    })
+})
+
+function buildMsg(name, text) {
+    return {
+        name,
+        text,
+        time: new Intl.DateTimeFormat('default', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        }).format(new Date())
+    }
+}
+
+//  для пользователя
+function activateUser(id, name, room) {
+    const user = { id, name, room }
+    UsersState.setUsers([
+        ...UsersState.users.filter(user => user.id !== id),
+        user
+    ])
+    return user
+}
+
+function userLeavesApp(id) {
+    UsersState.setUsers(
+        UsersState.users.filter(user => user.id !== id)
+    )
+}
+
+function getUser(id) {
+    return UsersState.users.find(user => user.id === id)
+}
+
+function getUsersInRoom(room) {
+    return UsersState.users.filter(user => user.room === room)
+}
+
+function getAllActiveRooms() {
+    return Array.from(new Set(UsersState.users.map(user => user.room)))
+}
